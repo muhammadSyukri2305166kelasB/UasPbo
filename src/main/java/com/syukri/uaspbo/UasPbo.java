c500045801f36f18ff01b2f3e61164475594c4b6
mSyukri-upi <m.syukri@upi.edu>
2025-05-05 12:35:17 +0700

    first commit

---
+++ src/main/java/com/syukri/uaspbo/UasPbo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.syukri.uaspbo;
+
+/**
+ *
+ * @author HP
+ */
+public class UasPbo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
