69c8338183adfa780ef8344599f07a84f95a2ac2
AndikaMaskar <andika7496@gmail.com>
2025-05-28 10:51:58 +0700

    ganti penamaan element

--- src/main/java/com/syukri/uaspbo/menu/checkEnding.java
+++ src/main/java/com/syukri/uaspbo/menu/checkEnding.java
@@ -4,10 +4,10 @@ public class checkEnding {
     public static boolean hasWon(java.util.Map<String, java.util.Set<String>> monsterCounter) {
         if (monsterCounter == null) return false;
 
-        return monsterCounter.getOrDefault("Basic", java.util.Collections.emptySet()).size() >= 3 &&
-               monsterCounter.getOrDefault("Fire", java.util.Collections.emptySet()).size() >= 3 &&
-               monsterCounter.getOrDefault("Plant", java.util.Collections.emptySet()).size() >= 3 &&
-               monsterCounter.getOrDefault("Water", java.util.Collections.emptySet()).size() >= 3;
+        return monsterCounter.getOrDefault("none", java.util.Collections.emptySet()).size() >= 3 &&
+               monsterCounter.getOrDefault("fire", java.util.Collections.emptySet()).size() >= 3 &&
+               monsterCounter.getOrDefault("plant", java.util.Collections.emptySet()).size() >= 3 &&
+               monsterCounter.getOrDefault("water", java.util.Collections.emptySet()).size() >= 3;
     }
 
     public static boolean hasLost(int dayCounter) {
