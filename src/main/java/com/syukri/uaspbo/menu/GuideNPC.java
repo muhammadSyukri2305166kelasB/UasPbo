ee07c06c67a79c4a4f2a445aedd9ed435e8240fa
ZephryVision <fajar.tengku23@gmail.com>
2025-05-31 21:00:40 +0700

    menambah dialog NPC day 4 - 12

--- src/main/java/com/syukri/uaspbo/menu/GuideNPC.java
+++ src/main/java/com/syukri/uaspbo/menu/GuideNPC.java
@@ -28,7 +28,7 @@ public class GuideNPC {
             System.out.println("12 pokemon yang ada untuk mengalahkan raja pokemon yang akan datang dalam 14 hari");
             System.out.println("lagi.");
             Battle.waitAndClear();
-            System.out.println("Traveler : ");
+            System.out.println("Traveller : ");
             System.out.println("Sepertinya ini tugas yang sangat berat.... Tapi, baiklah akan saya coba untuk mengangkap");
             System.out.println("12 pokemon kurang dari 14 hari.");
             Battle.waitAndClear();
@@ -36,7 +36,7 @@ public class GuideNPC {
             System.out.println("Di hari pertama ini, saya akan memberikan kamu petunjuk untuk cara menangkap pokemon yang ");
             System.out.println("benar");
             Battle.waitAndClear();
-            System.out.println("Traveler : ");
+            System.out.println("Traveller : ");
             System.out.println("Mohon bantuannya...........");
             Battle.waitAndClear();
             System.out.println("Digga : ");
@@ -49,17 +49,59 @@ public class GuideNPC {
             System.out.println("Tugas kita adalah membuat strategi agar bisa melawan semua pokemon setiap harinya dan memenangkan pertarungannya.");
             System.out.println("Lakukan setiap hari sebelum hari ke 14 tiba!!!!!");
             Battle.waitAndClear();
-            System.out.println("Traveler : ");
+            System.out.println("Traveller : ");
             System.out.println("Baiklah....terima kasih atas petunjukunya");
             Battle.waitAndClear();
         }else if(dayCounter == 2){
-            
+            System.out.println("Digga : ");
+            System.out.println("Selamat, Traveler. kamu telah menjalani hari pertamamu dengan baik.");
+            System.out.println("Kamu telah ke mengumpulkan pokemon dari hutan, gunung dan sungai dan Ascend pokemon yang kamu miliki");
+            System.out.println("hari ini dan selanjutnya kamu akan tetap menjelajahi banyak tempat dan mencoba melawan banyak pokemon");
+            System.out.println("yang lebih kuat dan mencoba menanngkap mereka");
+            Battle.waitAndClear();
+            System.out.println("Traveller :");
+            System.out.println("baiklah, cukup sulit ternyata. aku kira bakal mudah :v");
+            Battle.waitAndClear();
+            System.out.println("Digga : ");
+            System.out.println("Oh iya. jangan lupa untuk selalu melevel up pokemonmu. karena selanjutnya tantangannya akan lebih sulit.");
+            Battle.waitAndClear();
+            System.out.println("Traveller : ");
+            System.out.println("alamak...");
         }else if(dayCounter == 4){
+            System.out.println("Digga : ");
+            System.out.println("Bagaimana, Traveler apakah kamu sudah mengumpulkan banyak pokemon? oh iya aku ingat kalau");
+            System.out.println("sedang mencari-cari ayahmu juga kan.");
+            Battle.waitAndClear();
+            System.out.println("Traveller : ");
+            System.out.println("Iya, sih. ibuku berpesan kalau untuk mengetahui siapa ayahku maka aku harus mengumpulkan banyak");
+            System.out.println("pokemon. aku tidak mengerti apa maksudnya");
+            Battle.waitAndClear();
+            System.out.println("Digga : ");
+            System.out.println("hhhmmmppp...mungkinkah..");
+            Battle.waitAndClear();
+            System.out.println("Traveller : ");
+            System.out.println("...Apakah kamu tahu sesuatu?");
+            Battle.waitAndClear();
+            System.out.println("Digga : ");
+            System.out.println("Ntah lah. aku merasa aku tau sesuatu tapi aku lupa. oh iya. jangan berhenti untuk mengumpulkan");
+            System.out.println("pokemon karena kelompok aneh tersebut sudah berhasil menangkap sebagian pokemon");
 
         }else if(dayCounter == 7){
+            System.out.println("Traveller : ");
+            System.out.println("Digga, apa yang sebenarnya kelompok aneh cari dengan mengumpulkan seluruh pokemon?");
+            Battle.waitAndClear();
+            System.out.println("Digga : ");
+            System.out.println("Mereke ingin memanggil Dewa Pokemon. Jadi Dewa Pokemon adalah pokemon yang memiliki kekuatan luar biasa");
+            System.out.println("yang bisa menghancurkan, memanipulasi, menguasai banyak elemen. Pokemon ini adalah makhluk kuno yang menjadi");
+            System.out.println("sumber kekuatan para pokemon-pokemon yang ada saat ini. tentunya makhluk ini memmecahkan kekuatannya yang menjadikan");
+            System.out.println("pokemon yang kita kenal ada. Dengan mengumpulkan seluruh pokemon mereka akan berencana menggunakan kekuatan");
+            System.out.println("Dewa Pokemon untuk menguasai dunia.");
 
         }else if(dayCounter == 12){
-            
+            System.out.println("Digga : ");
+            System.out.println("Bagus, kamu sudah sampai sejauh ini. Nasib dunia dan para pokemon ada di tanganmu.");
+            System.out.println("Mereka, kelompok aneh itu, juga sudah hampir mencapai target mereka.");
+            System.out.println("Engkau pasti bisa melakukannya menyelamatkan para pokemon, dunia, dan menemukan siapa ayahmu");
         }
     }
 
